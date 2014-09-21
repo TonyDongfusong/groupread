744192af239e33e1eced50c8b63a8b069c797881
Cui Liqiang <cui.liqiang@gmail.com>
2014-09-21 11:00:25 +0800

    add oauth callback url
    change gem source to rubygems; config db url
    
    add vendors and move sqlite to development group
    
    add sqlite back
    
    try remove sqlite again..
    
    change Gemfile accordingly

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,8 @@
+class SessionController < ApplicationController
+	def new
+	end
+
+	def create
+		render text:"works"
+	end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+print File.expand_path('../boot', __FILE__)
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
